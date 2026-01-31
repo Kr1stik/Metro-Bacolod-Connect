39a200df2b9f81252c6dffd370c071d0df8fe999
Wenard Roy Barrera <wenardroybarrera@gmail.com>
2026-01-31 23:05:24 +0800

    Updated security in firebase

--- metro-bacolod-web/src/firebase-config.ts
+++ metro-bacolod-web/src/firebase-config.ts
@@ -1,19 +1,19 @@
-// src/firebase-config.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfg3Dnr-YS0PbLa6gzutaA-xbGWJijlwM",
-  authDomain: "metro-bacolod-connect.firebaseapp.com",
-  projectId: "metro-bacolod-connect",
-  storageBucket: "metro-bacolod-connect.firebasestorage.app",
-  messagingSenderId: "691134114686",
-  appId: "1:691134114686:web:679290269b1795abfba12f",
-  measurementId: "G-2WX8G2G32C"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
